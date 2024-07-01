456230d9c8a8aa823a333b423be0c755ad1c7c68
smateos004 <24373540+smateos004@users.noreply.github.com>
2024-07-01 12:53:19 +0200

    Update data_preparation.py

--- src/data_preparation.py
+++ src/data_preparation.py
@@ -1,1 +1,29 @@
+import pandas as pd
+import geopandas as gpd
 
+def load_data(filepath):
+    """
+    Carga los datos desde un archivo CSV y los devuelve como un DataFrame.
+    """
+    return pd.read_csv(filepath)
+
+def clean_data(df):
+    """
+    Realiza la limpieza de datos, eliminando filas y columnas innecesarias, y manejando valores nulos.
+    """
+    df = df.dropna()  # Ejemplo de limpieza
+    return df
+
+def save_clean_data(df, filepath):
+    """
+    Guarda el DataFrame limpio en un archivo CSV.
+    """
+    df.to_csv(filepath, index=False)
+
+if __name__ == "__main__":
+    raw_data_path = '../data/raw/mobility_data.csv'
+    processed_data_path = '../data/processed/clean_mobility_data.csv'
+    
+    data = load_data(raw_data_path)
+    clean_data = clean_data(data)
+    save_clean_data(clean_data, processed_data_path)
