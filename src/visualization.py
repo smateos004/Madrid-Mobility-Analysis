61962b6673eeafb863bca7e37d8212d98e9ba6cf
smateos004 <24373540+smateos004@users.noreply.github.com>
2024-07-01 12:54:24 +0200

    Update visualization.py

--- src/visualization.py
+++ src/visualization.py
@@ -1,1 +1,27 @@
+import pandas as pd
+import folium
+import geopandas as gpd
 
+def create_heatmap(df, output_path):
+    """
+    Crea un mapa de calor basado en los datos de rutas.
+    """
+    madrid_map = folium.Map(location=[40.4168, -3.7038], zoom_start=12)
+    
+    for _, row in df.iterrows():
+        folium.CircleMarker(
+            location=[row['latitude'], row['longitude']],
+            radius=5,
+            weight=2,
+            color='blue'
+        ).add_to(madrid_map)
+    
+    madrid_map.save(output_path)
+
+if __name__ == "__main__":
+    processed_data_path = '../data/processed/clean_mobility_data.csv'
+    heatmap_output_path = '../data/processed/madrid_heatmap.html'
+    
+    data = pd.read_csv(processed_data_path)
+    
+    create_heatmap(data, heatmap_output_path)
